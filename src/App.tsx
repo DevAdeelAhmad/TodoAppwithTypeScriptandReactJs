01fdb9866cc55422a3be8d36bb6639ab348b48f5
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-07-02 18:31:54 +0500

    checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import Todo from './components/Todo';
+import React from "react";
+import Todo from "./components/Todo";
 
 const myTodoItems = [
-    {
-        id : 1,
-        title : "Finish Homework"
-    },
-    {
-        id : 2,
-        title : "Eat Dinner and Spend Time with Family"
-    },
-    {
-        id : 3,
-        title : "Code, Sleep and Repeat"
-    }
-]
+  {
+    id: 1,
+    title: "Finish Homework",
+  },
+  {
+    id: 2,
+    title: "Eat Dinner and Spend Time with Family",
+  },
+  {
+    id: 3,
+    title: "Code, Sleep and Repeat",
+  },
+];
 
 const App: React.FC = () => {
-    return (
-        <div>
-            <Todo items={myTodoItems}/>
-        </div>
-    )
+  return (
+    <div>
+      <Todo items={myTodoItems} />
+    </div>
+  );
 };
 
 export default App;
