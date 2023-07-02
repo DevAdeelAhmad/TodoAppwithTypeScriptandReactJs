d18adc76db3072e360b63ea2987c062071d131ab
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-07-03 00:58:32 +0500

    counter using usestate

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,13 +1,20 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 const Counter: React.FC = (props) => {
-const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0);
+  const handleIncrement = () => {
+    setValue(value + 1);
+  }
+  const handleDecrement = () => {
+    if(value === 0) return;
+    setValue(value - 1);
+  }
   return (
     <div className="counter-container">
-      <h1>0</h1>
-      <button onClick={} style={{background : 'green'}}>INC</button>
-      <button style={{background : 'tomato'}}>DEC</button>
+      <h1>{value}</h1>
+      <button onClick={handleIncrement} style={{ background: "green" }}>INC</button>
+      <button onClick={handleDecrement} style={{ background: "tomato" }}>DEC</button>
     </div>
   );
 };
